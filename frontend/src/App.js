98fdaacdf406068b68b7ebc89a6628305fb02644
AlexM5869 <abm3273@utexas.edu>
2024-04-03 23:35:41 -0500

    Added AWS API code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,86 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import axios from 'axios';
 
-const mockVocab = [
-  {japanese: "猫", english: "Cat", passed: false},
-  {japanese: "犬", english: "Dog", passed: false}
-]
+// const mockVocab = [
+//   {japanese: "猫", english: "Cat", passed: false},
+//   {japanese: "犬", english: "Dog", passed: false}
+// ]
 
 function App() {
 
-  const [vocab, setVocab] = useState(mockVocab);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  ///// Component States /////
+  const [vocab, setVocab] = useState(null);
+  const [currentID, setCurrentID] = useState(0);
   const [showEnglish, setShowEnglish] = useState(false);
+  ////////////////////////////
 
-  const handleShowToggle = () => setShowEnglish(!showEnglish);
+  ///// AWS API Handling /////
+  const fetchVocabWord = async (wordId) => {
 
-  const handlePass = () => {
+    const apiURL = `https://8yjjcdl4u1.execute-api.us-east-2.amazonaws.com/$default/vocab/word?param1=${wordId}`;
+
+    try {
+
+      const response = await axios.get(apiURL);
+      const wordData = response.data
+
+      setVocab({japanese: wordData.japaneseWord, english: wordData.englishTranslation})
+
+    } catch (error) {
+
+      console.error(error);
+
+    }
+
+  }
+  ////////////////////////////
+
+  ///// Function Handling /////
+  const handleShowToggle = useCallback(() => {
+    setShowEnglish(!showEnglish);
+  }, [showEnglish]);
+
+  const handlePass = useCallback(() => {
 
     setShowEnglish(false); // Hide English for next word
 
-    if (currentIndex === 1) {
-      setCurrentIndex(0);
+    if (currentID === 1) {
+      setCurrentID(0);
     } else {
-      setCurrentIndex(currentIndex + 1);
+      setCurrentID(currentID + 1);
     }
 
-  };
+    fetchVocabWord(currentID.toString());
 
-  const handleFail = () => {
-    // Retry logic
+  }, [currentID])
+
+  const handleFail = useCallback(() => {
+
     setShowEnglish(false);
 
-    if (currentIndex === 1) {
-      setCurrentIndex(0);
+    if (currentID === 1) {
+      setCurrentID(0);
     } else {
-      setCurrentIndex(currentIndex + 1);
+      setCurrentID(currentID + 1);
     }
-  };
 
+    fetchVocabWord(currentID.toString())
+
+  }, [currentID])
+  /////////////////////////////
+ 
+
+  ///// Use Effects /////
+
+  // Fetches first vocab word when main component loads
+  useEffect(() => {
+
+    fetchVocabWord(currentID.toString());
+
+  }, [currentID]);
+
+  ///// Handles keyboard listeners /////
   useEffect(() => {
 
     const handleKeyPress = (e) => {
@@ -63,21 +107,23 @@ function App() {
 
     };
 
-  }, [showEnglish, currentIndex]);
+  }, [showEnglish, currentID, handleShowToggle, handlePass, handleFail]);
+  //////////////////////////////////////
 
   return (
 
     <div className='App'>
-      <div className='word'>{vocab[currentIndex].japanese}</div>
-      {showEnglish && <div className='english'>{vocab[currentIndex].english}</div>}
+      <div className='word'>{vocab?.japanese}</div>
+      <hr width="90%"></hr>
+      {showEnglish && <div className='english'>{vocab?.english}</div>}
       {!showEnglish ? (
         <div className='buttons'>
           <button onClick={handleShowToggle}>Show</button>
         </div>
       ) : (
         <div className='buttons'>
-          <button onClick={handlePass}>Pass</button>
-          <button onClick={handleFail}>Fail</button>
+          <button onClick={handlePass} className='pass'>Pass</button>
+          <button onClick={handleFail} className='fail'>Fail</button>
         </div>
       )}
     </div>
