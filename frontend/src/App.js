32120b59037838908be2646550e588a19334f543
AlexM5869 <abm3273@utexas.edu>
2024-04-03 19:32:48 -0500

    Added basic vocab frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
+const mockVocab = [
+  {japanese: "猫", english: "Cat", passed: false},
+  {japanese: "犬", english: "Dog", passed: false}
+]
 
 function App() {
+
+  const [vocab, setVocab] = useState(mockVocab);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showEnglish, setShowEnglish] = useState(false);
+
+  const handleShowToggle = () => setShowEnglish(!showEnglish);
+
+  const handlePass = () => {
+
+    setShowEnglish(false); // Hide English for next word
+
+    if (currentIndex === 1) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
+
+  };
+
+  const handleFail = () => {
+    // Retry logic
+    setShowEnglish(false);
+
+    if (currentIndex === 1) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  useEffect(() => {
+
+    const handleKeyPress = (e) => {
+
+      if (e.code === "Space") {
+
+        if (showEnglish === false) {
+          handleShowToggle();
+        } else if (showEnglish === true) {
+          handlePass();
+        }
+
+      } else if (e.code === "KeyX") {
+        handleFail();
+      } 
+
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return() => {
+      
+      window.removeEventListener('keydown', handleKeyPress);
+
+    };
+
+  }, [showEnglish, currentIndex]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='App'>
+      <div className='word'>{vocab[currentIndex].japanese}</div>
+      {showEnglish && <div className='english'>{vocab[currentIndex].english}</div>}
+      {!showEnglish ? (
+        <div className='buttons'>
+          <button onClick={handleShowToggle}>Show</button>
+        </div>
+      ) : (
+        <div className='buttons'>
+          <button onClick={handlePass}>Pass</button>
+          <button onClick={handleFail}>Fail</button>
+        </div>
+      )}
     </div>
+
   );
 }
 
